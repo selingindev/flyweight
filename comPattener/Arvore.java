eecfb6f9cb34c59d4bbf41e7dc222874bd62fa6b
selingindev <88356338+selingindev@users.noreply.github.com>
2025-10-23 12:05:48 -0300

    arvore com pattener

--- comPattener/Arvore.java
+++ comPattener/Arvore.java
@@ -1,3 +1,16 @@
 public class Arvore {
-    
+      public double x;
+      public double y;
+      public TypeArvore type;
+
+      public Arvore(double x, double y, TypeArvore type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+      }
+
+      void desenhar(){
+        type.desenhar(x, y);
+      }
+
 }
